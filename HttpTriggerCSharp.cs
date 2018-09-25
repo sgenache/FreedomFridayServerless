1f3aadceb1ce3ec0047a5e1011ad5f7e5aa83dff
sergiu240 <sergiu240@gmail.com>
2018-09-25 21:52:00 +0300

    added basic GraphQL functionality

--- HttpTriggerCSharp.cs
+++ HttpTriggerCSharp.cs
@@ -8,25 +8,59 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using GraphQL.Types;
+using GraphQL;
+using GraphQL.Http;
+using System.Linq;
 
 namespace FreedomFridayServerless.Function
 {
     public static class HttpTriggerCSharp
     {
+        private static readonly IDocumentExecuter _executer = new DocumentExecuter();
+        private static readonly IDocumentWriter _writer = new DocumentWriter();
+        private static readonly ISchema _schema = new Schema { Query = new StarWarsQuery() }; 
+
         [FunctionName("HttpTriggerCSharp")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function,"post", Route = "graphql")]HttpRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var request = Deserialize<GraphQLRequest>(req.Body);
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            var result = await _executer.ExecuteAsync(_ =>
+            {
+                _.Schema = _schema;
+                _.Query = request.Query;
+                _.OperationName = request.OperationName;
+                _.Inputs = request.Variables.ToInputs();
+                //_.UserContext = _settings.BuildUserContext?.Invoke(context);
+            });
+
+            var json = _writer.Write(result);
+            return result.Errors?.Any() == true 
+                ? new BadRequestObjectResult(json) 
+                : (ActionResult)new OkObjectResult(json);
         }
+
+        private static T Deserialize<T>(Stream s)
+        {
+            using (var reader = new StreamReader(s))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var ser = new JsonSerializer();
+                return ser.Deserialize<T>(jsonReader);
+            }
+        }  
+    }
+
+    public class GraphQLRequest
+    {
+        public string OperationName { get; set; }
+        public string Query { get; set; }
+        public JObject Variables { get; set; }
     }
 }
